36998f11fd03599c014dd70a29cce1bbdb0ebedc
domainry <319426245+domainry@users.noreply.github.com>
2026-08-23 17:21:50 +0800

    chore: identify first sdk development release

--- identity_test.go
+++ identity_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestCurrentIdentity(t *testing.T) {
 	want := Identity{
-		SDKVersion:      "v0.1.0-dev",
+		SDKVersion:      "v0.1.0-dev.1",
 		ContractVersion: "connector-contract-v1",
 		ContractSHA256:  "52e77dae1a2b09f210a2ee9597fe3bcbecb4ab875336e06a4a18ad3dfbcc90fb",
 	}
